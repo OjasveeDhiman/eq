4ce8def46986550de71b2a536a728a43f0c9cc5a
Ojasvee Dhiman <ojasvee_dhiman@yahoo.co.in>
2021-02-03 14:02:34 -0500

    eq

--- middleware/queryhandler.js
+++ middleware/queryhandler.js
@@ -1,5 +1,12 @@
 const {Pool} = require('pg');
-const pool = new Pool();
+require('dotenv').config();
+const pool =  new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
 exports.queryhandler = (req, res, next) => {
     pool.query(req.sqlQuery).then((r) => {
       return res.json(r.rows || [])
