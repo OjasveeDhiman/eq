bd2c133fcec4025aaa7520e4bca7c5f250ec2e0d
OjasveeDhiman <41508805+OjasveeDhiman@users.noreply.github.com>
2021-02-04 10:24:00 -0500

    Update queryhandler.js

--- middleware/queryhandler.js
+++ middleware/queryhandler.js
@@ -1,9 +1,9 @@
 const {Pool} = require('pg');
 const pool = new Pool(
-      user: 'readonly',
-      host: 'work-samples-db.cx4wctygygyq.us-east-1.rds.amazonaws.com',
-      database: 'work_samples',
-      password: 'w2UIO@#bg532!',
+      user: readonly,
+      host: work-samples-db.cx4wctygygyq.us-east-1.rds.amazonaws.com,
+      database: work_samples,
+      password: w2UIO@#bg532!,
       port: 5432
 );
 exports.queryhandler = (req, res, next) => {
